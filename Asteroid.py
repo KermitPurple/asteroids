e97c9a3b39304b7bfa26e141a2c880f92d19566f
Shane McDonough <shanemcdman@gmail.com>
2020-03-21 23:17:39 -0700

    Initial

---
+++ Asteroid.py
@@ -0,0 +1,22 @@
+import pygame
+from random import random, randint
+from numpy import sin, cos
+from collections import namedtuple
+
+Coord = namedtuple("Coord", ('x', 'y'))
+
+class Asteroid:
+
+    def __init__(self, screen, size, r, pos = Coord(0,0), vel = Coord(0,0)):
+        self.screen = screen
+        self.size = size
+        self.r = r
+        self.pos = pos
+        self.vel = vel
+
+    def randomPos(self):
+        self.pos = Coord(randint(0, self.size.x), randint(0, self.size.y))
+    
+    def randomVel(self):
+        theta = random() * 5
+        self.vel = Coord(cos(theta), sin(theta))
---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame, os
+from Asteroid import Asteroid, Coord
+os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
+
+pygame.display.init()
+size = Coord(600, 600)
+screen = pygame.display.set_mode(size)
+asteroids = []
+for i in range(1):
+    asteroids.append(Asteroid(screen, size, 20))
+    asteroids[i].randomPos()
+    asteroids[i].randomVel()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0,0,0))
+    pygame.display.update()
+
